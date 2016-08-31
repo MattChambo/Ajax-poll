3de879c2660bf98a49385eb002b71d933aa6aa76
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-09-01 09:57:43 +1200

    Add messages

--- public_html/api/poll.php
+++ public_html/api/poll.php
@@ -19,7 +19,7 @@ if($isValid == false){
 	];
 
 	// Convert the message into JSON
-	$message-> json_encode($message);
+	$message=json_encode($message);
 
 	// Prepare the header
 	header('Content-Type: application/json');
@@ -31,13 +31,37 @@ if($isValid == false){
 
 // Get the users IP address
 // For testing I am commenting this line out
-//$ipaddress = $_SERVER['REMOTE_ADDR'];
+$ipaddress = $_SERVER['REMOTE_ADDR'];
 
 // For testing, create random number to pretend we are using a different ip address
-$ipaddress = rand() . "\n";
+// $ipaddress = rand() . "\n";
 
 // Connect to database
 $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check to make sure the voter has not voted before
+$sql = "SELECT ip_address FROM vote WHERE ip_address = '$ipaddress'";
+
+// Run the query
+$result = $dbc->query($sql);
+
+// Count the number of records returned
+if($result->num_rows >= 1){
+
+	// Prepare the message
+	$message = [
+		'status' => false,
+		'message' => 'You cannot vote more than once'
+	];
+
+	//prepare the header
+	header('Content-Type: application/json');
+
+	echo json_encode($message);
+
+	// Stop
+	exit();
+}
 
 // Prepare the insert query
 $sql = "INSERT INTO vote VALUES (null, '$vote', '$ipaddress')";
@@ -66,5 +90,6 @@ if($dbc->affected_rows == 1){
 	// Prepare the header
 	header('Content-Type: application/json');
 
+	// Turn variable into json and then send to js
 	echo json_encode($message);
 }
